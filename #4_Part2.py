0dfaaa172ced2b890d9c72cd846b0544dabcf154
AD67ERI <119736421+AD67ERI@users.noreply.github.com>
2022-12-04 10:43:55 +0100

    Add files via upload

---
+++ #4_Part2.py
@@ -0,0 +1,23 @@
+#Ouvrir et lire le fichier d'inputs
+fichier=open("Input#4.txt", "r")
+lignes=fichier.readlines()
+
+nb_overlaps = 0
+
+for ligne in lignes :
+    
+    #Création et concaténation des listes de sections assignées à chacun des deux elfs
+    l=ligne.split(',')
+    l[1]=l[1][:(len(l[1])-1)]
+    list=l[0].split('-') + l[1].split('-')
+    sections=[i for i in range(int(list[0]),int(list[1])+1)] + [i for i in range(int(list[2]),int(list[3])+1)]
+    
+    #Test d'overlap :
+    nb_doubles = 0
+    for i in sections :
+        if sections.count(i) >= 2 :
+            nb_doubles += 1
+    if nb_doubles >= 1 :
+        nb_overlaps += 1 
+    
+print(nb_overlaps)
